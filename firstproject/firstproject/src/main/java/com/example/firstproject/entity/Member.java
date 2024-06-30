01fd42eab93db280a44bc29eb600c35e4c289d46
Re-Note <dldydtn207@gmail.com>
2024-07-01 04:57:06 +0900

    step4

--- firstproject/firstproject/src/main/java/com/example/firstproject/entity/Member.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/entity/Member.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
 public class Member {
     @Id
     @GeneratedValue
@@ -14,19 +22,5 @@ public class Member {
     String email;
     @Column
     String password;
-
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+    // Member 생성자와 toString() 메서드 삭제
 }
